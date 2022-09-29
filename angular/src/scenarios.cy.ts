378c735d53fb33d5fb8b8e886ae86458a1307907
Rocky <25568640+rockindahizzy@users.noreply.github.com>
2022-09-29 13:59:35 -0500

    Add angular

---
+++ angular/src/scenarios.cy.ts
@@ -0,0 +1,36 @@
+import { AppComponent } from './app/app.component'
+
+describe("Scenarios", () => {
+  // it("default", () => {
+  //   cy.mount(AppComponent);
+  // });
+  // it('eslint warning', () => {
+  //   5 + 5;
+  //   cy.mount(AppComponent);
+  // })
+  // it("eslint error", () => {
+  //   let thing = "Hello World!";
+  //   cy.mount(AppComponent);
+  // });
+  // it('typescript warning', () => {
+  //   let thing: number = 'Hello World'
+  //   if (thing) {
+  //     cy.mount(AppComponent);
+  //   }
+  // })
+  // it('typescript error', () => {
+  //   // Angular will fix small mistakes?!?!?!?! (){} gets converted to () => {}
+  //   let thing = ()/(={}
+  //   cy.mount(AppComponent);
+  // })
+  // it('runtime error in spec', () => {
+  //   throw new Error('uh oh')
+  //   cy.mount(AppComponent);
+  // })
+  // it("runtime error in component", () => {
+  //   cy.mount(AppComponent);
+  //   cy.get('button').contains('Throw Error').click();
+  //   // Error is not displayed in spec window
+  // });
+});
+
